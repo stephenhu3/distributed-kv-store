06ee5763b13c86bc8c15080dcfb2f6c5124544b9
stephenhu3 <stephenhu3@gmail.com>
2017-01-08 18:49:26 -0800

    expanded UDPRequest

--- CPEN431/src/main/java/A1/core/UDPRequest.java
+++ CPEN431/src/main/java/A1/core/UDPRequest.java
@@ -1,9 +1,9 @@
 package A1.core;
 
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.security.NoSuchAlgorithmException;
@@ -15,6 +15,9 @@ public class UDPRequest {
     private static final int MAX_MSG_SIZE = 16384;
     private static final int UNIQUE_ID_SIZE = 16;
     private static final int SECRET_CODE_LEN_SIZE = 4;
+    // default timeout of 100ms
+    private static final int TIMEOUT = 100;
+    private static final int MAX_RETRIES = 3;
 
     public static byte[] generateRequest(int snum, byte[] uniqueID) throws NoSuchAlgorithmException {
         // Maximum payload size 16KB
@@ -42,43 +45,51 @@ public class UDPRequest {
         return uniqueId;
     }
 
-    public static byte[] sendRequest(String ip, int port, int snum)
-            throws IOException, NoSuchAlgorithmException {
+    public static byte[] sendRequest(String ip, int port, int snum) throws Exception {
         DatagramSocket socket = new DatagramSocket();
-
-        // send request
         byte[] uniqueID = generateUniqueID();
         byte[] req = generateRequest(snum, uniqueID);
         InetAddress address = InetAddress.getByName(ip);
         DatagramPacket reqPacket = new DatagramPacket(req, req.length, address, port);
-        socket.send(reqPacket);
-
-        /*
-        First 16 bytes are the unique ID of the corresponding request, rest is the application level
-        payload. Maximum payload size 16KB.
-        Payload has format: Secret code length (integer, 4 bytes, big-endian),
-        Secret code (byte array), Possible padding (to be ignored)
-        */
-
         DatagramPacket resPacket = new DatagramPacket(req, req.length);
-        socket.receive(resPacket);
 
-        // check matching uniqueID
-        byte[] res = resPacket.getData();
-        if (!Arrays.equals(Arrays.copyOf(res, UNIQUE_ID_SIZE), uniqueID)) {
-            System.out.println("uniqueID not matching");
-            return null;
+        for (int i = 0, timeoutMs = TIMEOUT; i <= MAX_RETRIES; i++, timeoutMs*=2) {
+            socket.setSoTimeout(timeoutMs);
+            // send request
+            System.out.println("Sending packet");
+            socket.send(reqPacket);
+
+            /*
+            First 16 bytes are the unique ID of the corresponding request, rest is the application level
+            payload. Maximum payload size 16KB.
+            Payload has format: Secret code length (integer, 4 bytes, big-endian),
+            Secret code (byte array), Possible padding (to be ignored)
+            */
+
+            System.out.println("Receiving packet");
+            try {
+                socket.receive(resPacket);
+            } catch(SocketTimeoutException e) {
+                continue;
+            }
+
+            // check matching uniqueID
+            byte[] res = resPacket.getData();
+            if (!Arrays.equals(Arrays.copyOf(res, UNIQUE_ID_SIZE), uniqueID)) {
+                System.out.println("uniqueID not matching");
+                throw new Exception("Mismatched uniqueID");
+            }
+
+            ByteBuffer byteBuffer = ByteBuffer.allocate(SECRET_CODE_LEN_SIZE);
+            byteBuffer.limit(SECRET_CODE_LEN_SIZE);
+            byteBuffer.order(ByteOrder.BIG_ENDIAN);
+            byteBuffer.put(resPacket.getData());
+            int secretCodeLength = byteBuffer.getInt();
+
+            System.out.println("Secret code length: " + SECRET_CODE_LEN_SIZE);
+
+            socket.close();
         }
-
-        ByteBuffer byteBuffer = ByteBuffer.allocate(SECRET_CODE_LEN_SIZE);
-        byteBuffer.limit(SECRET_CODE_LEN_SIZE);
-        byteBuffer.order(ByteOrder.BIG_ENDIAN);
-        byteBuffer.put(resPacket.getData());
-        int secretCodeLength = byteBuffer.getInt();
-
-        System.out.println("Secret code length: " + SECRET_CODE_LEN_SIZE);
-
-        socket.close();
         return null;
     }
 }
