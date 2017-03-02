60f91f6d284525658b5b9edbcb13dd4a539c2de3
stephenhu3 <stephenhu3@gmail.com>
2017-03-02 13:53:16 -0800

    added sleep to GossipSenderThread so it only pushes every half second

--- CPEN431/src/main/java/A4/server/GossipSenderThread.java
+++ CPEN431/src/main/java/A4/server/GossipSenderThread.java
@@ -82,6 +82,12 @@ public class GossipSenderThread extends Thread {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            // Sleep so gossiping only performed every half second
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
