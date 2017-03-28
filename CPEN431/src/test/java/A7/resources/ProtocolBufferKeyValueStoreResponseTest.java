8107213f8961266b3ff134b5a5375bd34720ed63
Emmett Tan <UBCEmmettTan@gmail.com>
2017-03-28 01:49:42 -0700

    testGeneratePutResponse passes

--- CPEN431/src/test/java/A7/resources/ProtocolBufferKeyValueStoreResponseTest.java
+++ CPEN431/src/test/java/A7/resources/ProtocolBufferKeyValueStoreResponseTest.java
@@ -61,71 +61,71 @@ public class ProtocolBufferKeyValueStoreResponseTest {
     }
 
 
-//    @Test
-//    public void testGeneratePutDupesResponse() throws NoSuchAlgorithmException, IOException {
-//        Msg resMsg;
-//        ByteString value;
-//        ByteString messageId;
-//        KVRequest kvReq;
-//        ByteString key;
-//        int originalSize = KVStore.size();
-//
-//        assert(originalSize == 5);
-//
-//        key = ByteString.copyFrom("Key1".getBytes());
-//
-//
-//        sendReplication = new SendReplication(new MsgWrapper(null, InetAddress.getByName("localhost"), 11111) );
-//        final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-//        executor.schedule(sendReplication, 2, TimeUnit.SECONDS);
-//
-//        DatagramSocket socket = new DatagramSocket(11111);
-//        byte[] buf = new byte[MAX_MSG_SIZE];
-//        DatagramPacket reqPacket = new DatagramPacket(buf, buf.length);
-//        try {
-//            socket.receive(reqPacket);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        KVStore.clear();
-//        Msg request = null;
-//        try {
-//            request = Msg.parseFrom(
-//                    Arrays.copyOf(reqPacket.getData(), reqPacket.getLength()));
-//        } catch (InvalidProtocolBufferException e) {
-//            e.printStackTrace();
-//        }
-//
-//        ByteString currentID = request.getMessageID();
-//        byte[] payload = request.getPayload().toByteArray();
-//
-//        try {
-//            kvReq = KVRequest.parseFrom(payload);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            socket.close();
-//            return;
-//        }
-//
-//        assert(KVStore.size() == 0);
-//
-//        resMsg = ProtocolBufferKeyValueStoreResponse.generatePutDupesResponse(ByteString.copyFrom(kvReq.getValue().toByteArray()), currentID);
-//
-//        // verify checksum
-//        assertNotNull(request);
-//        assertEquals(request.getCheckSum(), calculateProtocolBufferChecksum(currentID.toByteArray(), payload));
-//        socket.close();
-//        assert(KVStore.size() == originalSize);
-//        assertEquals(KVStore.get(ByteString.copyFrom("Key1".getBytes())).getValue(), new VersionedValue(ByteString.copyFrom("Value1".getBytes()), 1).getValue());
-//        assertEquals(KVStore.get(ByteString.copyFrom("Key2".getBytes())).getValue(), new VersionedValue(ByteString.copyFrom("Value2".getBytes()), 2).getValue());
-//        assertEquals(KVStore.get(ByteString.copyFrom("Key3".getBytes())).getValue(), new VersionedValue(ByteString.copyFrom("Value3".getBytes()), 3).getValue());
-//        assertEquals(KVStore.get(ByteString.copyFrom("Key4".getBytes())).getValue(), new VersionedValue(ByteString.copyFrom("Value4".getBytes()), 4).getValue());
-//        assertEquals(KVStore.get(ByteString.copyFrom("Key5".getBytes())).getValue(), new VersionedValue(ByteString.copyFrom("Value5".getBytes()), 5).getValue());
-//        assertEquals(KVStore.get(ByteString.copyFrom("Key1".getBytes())).getVersion(), new VersionedValue(ByteString.copyFrom("Value1".getBytes()), 1).getVersion());
-//        assertEquals(KVStore.get(ByteString.copyFrom("Key2".getBytes())).getVersion(), new VersionedValue(ByteString.copyFrom("Value2".getBytes()), 2).getVersion());
-//        assertEquals(KVStore.get(ByteString.copyFrom("Key3".getBytes())).getVersion(), new VersionedValue(ByteString.copyFrom("Value3".getBytes()), 3).getVersion());
-//        assertEquals(KVStore.get(ByteString.copyFrom("Key4".getBytes())).getVersion(), new VersionedValue(ByteString.copyFrom("Value4".getBytes()), 4).getVersion());
-//        assertEquals(KVStore.get(ByteString.copyFrom("Key5".getBytes())).getVersion(), new VersionedValue(ByteString.copyFrom("Value5".getBytes()), 5).getVersion());
-//    }
+    @Test
+    public void testGeneratePutDupesResponse() throws NoSuchAlgorithmException, IOException {
+        Msg resMsg;
+        ByteString value;
+        ByteString messageId;
+        KVRequest kvReq;
+        ByteString key;
+        int originalSize = KVStore.size();
+
+        assert(originalSize > 0);
+
+        key = ByteString.copyFrom("Key1".getBytes());
+
+
+        sendReplication = new SendReplication(new MsgWrapper(null, InetAddress.getByName("localhost"), 11111) );
+        final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+        executor.schedule(sendReplication, 2, TimeUnit.SECONDS);
+
+        DatagramSocket socket = new DatagramSocket(11111);
+        byte[] buf = new byte[MAX_MSG_SIZE];
+        DatagramPacket reqPacket = new DatagramPacket(buf, buf.length);
+        try {
+            socket.receive(reqPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        KVStore.clear();
+        Msg request = null;
+        try {
+            request = Msg.parseFrom(
+                    Arrays.copyOf(reqPacket.getData(), reqPacket.getLength()));
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+
+        ByteString currentID = request.getMessageID();
+        byte[] payload = request.getPayload().toByteArray();
+
+        try {
+            kvReq = KVRequest.parseFrom(payload);
+        } catch (Exception e) {
+            e.printStackTrace();
+            socket.close();
+            return;
+        }
+
+        assert(KVStore.size() == 0);
+
+        resMsg = ProtocolBufferKeyValueStoreResponse.generatePutDupesResponse(ByteString.copyFrom(kvReq.getValue().toByteArray()), currentID);
+
+        // verify checksum
+        assertNotNull(request);
+        assertEquals(request.getCheckSum(), calculateProtocolBufferChecksum(currentID.toByteArray(), payload));
+        socket.close();
+        assert(KVStore.size() == originalSize);
+        assertEquals(KVStore.get(ByteString.copyFrom("Key1".getBytes())).getValue(), new VersionedValue(ByteString.copyFrom("Value1".getBytes()), 1).getValue());
+        assertEquals(KVStore.get(ByteString.copyFrom("Key2".getBytes())).getValue(), new VersionedValue(ByteString.copyFrom("Value2".getBytes()), 2).getValue());
+        assertEquals(KVStore.get(ByteString.copyFrom("Key3".getBytes())).getValue(), new VersionedValue(ByteString.copyFrom("Value3".getBytes()), 3).getValue());
+        assertEquals(KVStore.get(ByteString.copyFrom("Key4".getBytes())).getValue(), new VersionedValue(ByteString.copyFrom("Value4".getBytes()), 4).getValue());
+        assertEquals(KVStore.get(ByteString.copyFrom("Key5".getBytes())).getValue(), new VersionedValue(ByteString.copyFrom("Value5".getBytes()), 5).getValue());
+        assertEquals(KVStore.get(ByteString.copyFrom("Key1".getBytes())).getVersion(), new VersionedValue(ByteString.copyFrom("Value1".getBytes()), 1).getVersion());
+        assertEquals(KVStore.get(ByteString.copyFrom("Key2".getBytes())).getVersion(), new VersionedValue(ByteString.copyFrom("Value2".getBytes()), 2).getVersion());
+        assertEquals(KVStore.get(ByteString.copyFrom("Key3".getBytes())).getVersion(), new VersionedValue(ByteString.copyFrom("Value3".getBytes()), 3).getVersion());
+        assertEquals(KVStore.get(ByteString.copyFrom("Key4".getBytes())).getVersion(), new VersionedValue(ByteString.copyFrom("Value4".getBytes()), 4).getVersion());
+        assertEquals(KVStore.get(ByteString.copyFrom("Key5".getBytes())).getVersion(), new VersionedValue(ByteString.copyFrom("Value5".getBytes()), 5).getVersion());
+    }
 }
